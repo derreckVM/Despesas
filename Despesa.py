5226a08a5e0453302ac419fa921b1803dbb267de
derreck <derreck775@gmail.com>
2018-04-06 06:08:18 -0300

    Estrutura class Despesa

---
+++ Despesa.py
@@ -0,0 +1,47 @@
+
+
+
+
+class Despesa():
+
+    #variaveis privadas da classe
+    __valor;
+    __categoria;
+    __data;
+
+    #metodos para tratar a variavel valor
+    def validarValor(valor):
+        pass
+
+    def inserirValor(valor):
+        pass
+
+    def editarValor(valor):
+        pass
+
+    def retornarValor():
+        pass
+
+    #------------------------------------
+
+    #metodos para tratar a variavel categoria
+    def adicionarCategoria():
+        pass
+
+    def editarCategoria():
+        pass
+
+    def retornarCategoria():
+        pass
+
+    #------------------------------------
+
+    #metodos para tratar a variavel data
+    def editarData(data):
+        pass
+
+    def retornarData(data):
+        pass
+
+
+    
